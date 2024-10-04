3591fd7d8cd9e8f91e083b9d09ca64a4b6426d1f
13FHG37 <vadim777rex@abv.bg>
2024-10-04 12:23:36 +0300

    a

---
+++ Model/Workout.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Workout
+{
+	public Workout()
+	{
+        public int Id { get; set; }
+     public string Date { get; set; }
+     public int Duration { get; set; }
+     public string Type { get; set; }
+    }
+}
